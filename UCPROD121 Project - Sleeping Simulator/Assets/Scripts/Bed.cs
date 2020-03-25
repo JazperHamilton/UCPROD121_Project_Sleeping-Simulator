02e0ff75f66e053c89be5b185786a3601f7a81d6
JHamilton <jazper.hamilton@gmail.com>
2020-03-26 00:44:51 +1300

    Miscallanious things I forgot about.
    Sum stuff. I forget.

---
+++ UCPROD121 Project - Sleeping Simulator/Assets/Scripts/Bed.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed : MonoBehaviour
+{
+    public enum Beds {bed1, bed2, bed3}
+    public Beds whatBedAmI;
+}
---
+++ UCPROD121 Project - Sleeping Simulator/Assets/Scripts/PlayerRaycasting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycasting : MonoBehaviour
+{
+    public float distanceToSee;
+    RaycastHit whatIHit;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
+
+        if(Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("I toched " + whatIHit.collider.gameObject.name);
+                if (whatIHit.collider.tag == "Beds")
+                {
+                    if (whatIHit.collider.gameObject.GetComponent<Bed>().whatBedAmI == Bed.Beds.bed1)
+                    {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
